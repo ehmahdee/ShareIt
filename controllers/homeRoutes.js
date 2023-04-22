a7e567152b677121218a5d0195deb2a942e11d3a
Connor Raper-Mateo <connor.rapermateo@student.csulb.edu>
2023-04-21 17:04:22 -0700

    Testing seed.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { test } = require('node:test');
-const { Project, User } = require('../models');
+const {  User } = require('../models');
 const withAuth = require('../utils/auth');
 
 const li_client_id = process.env.LI_CLIENT_ID;
@@ -37,48 +37,18 @@ function litoken (query) {
 
 
 
-
-
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await User.findAll({
-    });
-
-    // Serialize data so the template can read it
-    // const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
+    
+    res.render('hub', {
       logged_in: req.session.logged_in 
     });
-    // res.json({key:process.env.FB_CLIENT_SECRET})
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/project/:id', async (req, res) => {
-  try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
 
-    const project = projectData.get({ plain: true });
-
-    res.render('project', {
-      ...project,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
@@ -86,7 +56,7 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
+      include: [{ model: Account }],
     });
 
     const user = userData.get({ plain: true });
@@ -103,7 +73,7 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/hub');
     return;
   }
 
